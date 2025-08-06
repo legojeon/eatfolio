4b40e2e1a2a119a3076169a514476b1b44fd2151
legojeon <legojeon@kaist.ac.kr>
2025-08-06 14:03:38 +0900

    camera

--- eatfolio/android/app/build.gradle.kts
+++ eatfolio/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.eatfolio"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
